48db545d8e9cb00074e5a163416c6c9b057f182f
seonbo.shim <seonbo.shim@bespinglobal.com>
2022-10-06 17:05:06 +0900

    ami 가 변경되더라도 ec2 를 다시 생성하지 않도록 ignore_changes 속성 추가

--- main.tf
+++ main.tf
@@ -108,6 +108,10 @@ resource "aws_instance" "this" {
     delete = lookup(var.timeouts, "delete", null)
   }
 
+  lifecycle {
+    ignore_changes = [ "ami" ]
+  }
+
   tags        = merge({ "Name" = var.name }, var.tags)
   volume_tags = var.enable_volume_tags ? merge({ "Name" = var.name }, var.volume_tags) : null
 }
